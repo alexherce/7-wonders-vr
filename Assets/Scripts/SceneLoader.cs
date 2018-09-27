c0e66a25f5b5005d32fbea4560cf02e9d4023785
halabe15 <halabe15@gmail.com>
2018-09-27 10:34:17 -0500

    petra

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour {
+	
+	public Object scene;
+
+	void OnTriggerEnter(Collider other)
+     {
+         if (other.tag == "Player") 
+         {
+			Application.LoadLevel(scene.name);
+         }
+     }
+}
